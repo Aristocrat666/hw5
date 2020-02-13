18b5652c5bb3161fa5b0af48abef099674ade9c3
levoschkin <levoschkin@IGORILIN>
2020-02-13 14:40:44 +0300

    rjvvbn 2

--- src/hw5.java
+++ src/hw5.java
@@ -6,7 +6,7 @@ public class hw5 {
         for (int i = 0; i < arr.length; i++)
             arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         return arr;
-    } 
+    }
 
     public void runOneThread() {
         float[] arr = new float[SIZE];
